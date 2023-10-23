bfd239de08aeb37eb931e54eb5a565c1b31f9396
devot-e <r23711268@gmail.com>
2023-10-23 19:10:33 +0530

    collected static

--- MediNavi/MediNavi/settings.py
+++ MediNavi/MediNavi/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'MediNavi.urls'
@@ -118,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / 'resources'
 
 STATIC_URL = 'static/'
 
